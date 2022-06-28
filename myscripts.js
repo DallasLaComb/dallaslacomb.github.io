65e740aa414d966f67230519483a7fb1fed1b824
Dallas LaComb <88401844+DallasLaComb@users.noreply.github.com>
2022-06-27 22:41:00 -0400

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -1,35 +1,39 @@
 //There is 45 game boxes - (5 x 9)
-var gameBoxes = document.getElementsByClassName("btn col-xs-2  ");
+if (window.matchMedia("(min-width: 768px)").matches) {
+  var gameBoxes = document.getElementsByClassName("btn");
+} else if (window.matchMedia("(max-width: 768px)").matches) {
+  var gameBoxes = document.getElementsByClassName("btn2");
+}
 
 var round; // Later will be used to determine the amount of blocks that appear on the screen.
 var lives; // Later will be used to see how many strikes you have. 3 Strikes and you lose and have to start over.
 var container = document.getElementById("container"); //Used to delete the game at the end of the round. It's the div the whole game is wrapped in.
 var roundCompleted = document.getElementsByClassName("continue").length == 0;
-
-if (roundCompleted) {
-  // update round
-  if (round == null) {
-    round = 4;
-  } else {
-    round++;
-  }
-  let buttonNumber = 1; //This variable assigns each button a number, starting with 1.
-  var counter = 0; //Used as a way to keep the while loop going untill all the boxes are filled with numbers for the round.
-
-  while (round <= 45 && counter < round) {
-    //Randomizes what buttons come up for the round.
-    let rng = Math.floor(Math.random() * 45);
-    if (gameBoxes[rng].classList.contains("active") == false) {
-      gameBoxes[rng].innerHTML = buttonNumber.toString();
-      gameBoxes[rng].classList.add("active");
-      gameBoxes[rng].id = buttonNumber.toString();
-      buttonNumber++;
-      counter++;
+window.onload = function () {
+  if (roundCompleted) {
+    // update round
+    if (round == null) {
+      round = 4;
     } else {
-      continue;
+      round++;
+    }
+    let buttonNumber = 1; //This variable assigns each button a number, starting with 1.
+    var counter = 0; //Used as a way to keep the while loop going untill all the boxes are filled with numbers for the round.
+
+    while (round <= 45 && counter < round) {
+      //Randomizes what buttons come up for the round.
+      let rng = Math.floor(Math.random() * 45);
+      if (gameBoxes[rng].classList.contains("active") == false) {
+        gameBoxes[rng].innerHTML = buttonNumber.toString();
+        gameBoxes[rng].classList.add("active");
+        gameBoxes[rng].id = buttonNumber.toString();
+        buttonNumber++;
+        counter++;
+      } else {
+        continue;
+      }
     }
   }
-
   var actives = document.getElementsByClassName("active"); //All currently active game boxes have the class "active".
   var boxNumber = 1; //Used as a way to track you're clicking the current boxes in order.
   $(".active").click(function () {
@@ -69,7 +73,7 @@ if (roundCompleted) {
       }
     }
   });
-}
+};
 
 $(function () {
   $("#nav-placeholder").load("navbar.html");
